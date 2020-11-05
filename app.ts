b54ad24c7da5c9a3a3992cd4e852b08f70209303
German <doguziagerman@gmail.com>
2020-11-05 15:10:00 +0300

    Добавил пару страниц и работу над ними

--- app.ts
+++ app.ts
@@ -1,39 +1,43 @@
-import { url } from "inspector"
-
 const fs = require('fs')
 const http = require('http')
+let cssFile
+
+
 
 http.createServer((req,res) => {
 
-    // switch (req.url){
+    switch(req.url){
 
-    //     case 'value1': 
-    //     break;
+        case '/style.css':
+            res.writeHead(200, {"Content-Type": "text/css"})
+            fs.readFile(process.cwd()+'/some_folder/' + 'style.css', 'utf-8', (err, data) => {
+                if (err) throw err
+                res.end(data)
+            }); break;  
 
-    //     case 'value1':
-    //     break;
+        case '/' :
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.readFile(process.cwd()+'/some_folder/' + 'index.html', 'utf-8', (err, data) => {
+                if (err) throw err
+                res.end(data);
+            });   break;
 
-    //     case 'value1':
-    //     break;
+        case '/some_page.html': 
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            fs.readFile(process.cwd() + '/some_folder/some_page.html', 'utf-8', (err,data) => {
+                if (err) throw err
+                res.end(data)
+            })
 
-    //     case 'value1':    
-    //     break;
-
-    // }
-
-    if (req.url === "/index.html" || req.url === "/") {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        fs.readFile(process.cwd()+'/some_folder/' + 'index.html', 'utf-8', (err, data) => {
-            if (err) throw err
-            res.end(data)
-        })
+        default: 
+            res.writeHead(404, {"Content-Type": "text/html"})
+            fs.readFile(process.cwd()+'/some_folder/' + '404.html', 'utf-8', (err,data) => {
+                if (err) throw err
+                res.end(data)
+                
+            }) ;    break;
     }
+
     
-    else {
-        res.writeHead(404, {"Content-Type": "text/plain"})
-        res.write("404 Not Found\n")
-        res.end()
-    }
 
-        
 }).listen(3000) 
