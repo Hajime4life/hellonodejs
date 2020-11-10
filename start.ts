b9181b7b85405dbf863e2612cce1601b178d7425
German <doguziagerman@gmail.com>
2020-11-10 19:02:29 +0300

    Попытка что-то изменить

--- start.ts
+++ start.ts
@@ -5,42 +5,52 @@ const path = require('path')
 console.log(`Server is working on http://localhost:3000`)
 
 http.createServer( (res,req) => {
-    
 
     let fold = '.' + req.url;
-
     if (fold == './') 
         fold = './index.html'
 
     let ex = path.extname(fold)
-
     let cType = 'text/html'
 
     switch(ex){
-
         case '.html':
             cType = 'text/html';
         break;
-
         case '.css':
             cType = 'text/css';
         break;
-
         case '.png':
             cType = 'text/png';
         break;
-
         case '.jpg':
             cType = 'text/jpg';
-        break;  
-        
+        break;     
         default: 
             cType = 'text/plain';
-        break;
-        
+        break; 
     }
     
-    res.writeHead(200, { 'Content-Type': cType });
+
+    fs.readFile(process.cwd() + fold, 'utf-8', (err, data) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                fs.readFile(process.cwd()+'/public/404.html', (err, data) => {
+                    res.writeHead(200, { 'Content-Type': cType });
+                    res.end(data, 'utf-8');
+                });
+            }   else {
+                res.writeHead(500)
+                res.write(`Sorry that's some server error!`)
+                res.end()
+            }
+        }
+
+        res.writeHead(200, { 'Content-Type': cType });
+        res.end();
+    })
+
+    
 
 
 }).listen(3000);
